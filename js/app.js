459fcbdfc64ccc6c2b5a47683a37a28433dc8054
v_duantao <v_duantao@baidu.com>
2018-03-21 21:08:37 +0800

    添加玩家

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 var Enemy = function() {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
+    this.x = 0;
+    this.y = 0;
 
     // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
@@ -22,10 +24,69 @@ Enemy.prototype.render = function() {
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 
+var Player = function () {
+    this.x = 200;
+    this.y = 400;
+
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function() {
+    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 都是以同样的速度运行的
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (keyCode) {
+    console.log(keyCode);
+    switch (keyCode) {
+        case 'left':
+            if (this.x <= 0) {
+                this.x = 0;
+            }
+            else {
+                this.x -= 100;
+            }
+            break;
+        case 'right':
+            if (this.x >= 400) {
+                this.x = 400;
+            }
+            else {
+                this.x += 100;
+            }
+            break;
+        case 'up':
+            if (this.y <= 0) {
+                this.y = 0;
+            }
+            else {
+                this.y -= 80;
+            }
+            break;
+        case 'down':
+            if (this.y >= 400) {
+                this.y = 400;
+            }
+            else {
+                this.y += 80;
+            }
+            break;
+    }
+    this.render();
+};
+
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
+var enemy = new Enemy();
+var allEnemies = [enemy];
+var player = new Player();
+
 
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
