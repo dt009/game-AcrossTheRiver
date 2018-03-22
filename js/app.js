2d3cca83e5b0eb0f909c96590e83ac0676958b43
v_duantao <v_duantao@baidu.com>
2018-03-22 20:36:05 +0800

    添加多个敌人,  使i敌人运动
    添加多个敌人,  使i敌人运动

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function (index) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-    this.x = 0;
-    this.y = 0;
+    this.position = [80, 160, 240];
+    this.x = -100;
+
+    this.index = index;
+
+    this.speed = Math.floor(Math.random() * 400) + 100;
+    this.y = this.position[Math.floor(Math.random() * 3)];
 
     // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
@@ -11,13 +16,15 @@ var Enemy = function() {
 
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
-Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed * dt;
+    if (this.x >= 500) {
+        allEnemies.splice(this.index, 1, new Enemy(this.index))
+    }
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -25,23 +32,22 @@ Enemy.prototype.render = function() {
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
 
 var Player = function () {
-    this.x = 200;
-    this.y = 400;
+    this.x = 0;
+    this.y = 0;
 
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function() {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
-    // 都是以同样的速度运行的
+Player.prototype.update = function () {
+
 };
 
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 Player.prototype.handleInput = function (keyCode) {
-    console.log(keyCode);
     switch (keyCode) {
         case 'left':
             if (this.x <= 0) {
@@ -76,22 +82,19 @@ Player.prototype.handleInput = function (keyCode) {
             }
             break;
     }
-    this.render();
 };
 
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
-var enemy = new Enemy();
-var allEnemies = [enemy];
+var allEnemies = [new Enemy(0), new Enemy(1), new Enemy(2), new Enemy(3)];
 var player = new Player();
-
 
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
 // 方法里面。你不需要再更改这段代码了。
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
